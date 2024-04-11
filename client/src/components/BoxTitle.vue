df23ab5e4b976a0f21c731eebdb9da986ae3cf39
Troy Ko <100921603+IcePhantomKo@users.noreply.github.com>
2024-04-11 16:29:54 +0800

    some change

---
+++ client/src/components/BoxTitle.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="boxTitle">
+        <dv-decoration-1 class = "dv3" />
+        <div class="boxTitle">{{ boxTitle }}</div>
+        <dv-decoration-1 class = "dv3" />
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        boxTitle:String,
+        required: true
+    }
+}
+</script>
+
+<style scoped>
+    .boxTitle{
+        display: flex;
+        justify-content: space-evenly;
+        white-space: nowrap;
+        font-size: medium;
+        letter-spacing: .1rem;
+        color: #01ffff;
+    }
+    .dv3{
+        flex:1;
+        width:10px;
+        height: 20px;
+    }
+</style>
--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -2,33 +2,32 @@
 <template>
     <div class="Main">
         <!-- First Line Info -->
-        <div class="box">
-            <dv-border-box-12>
-                <div class="boxTitle">
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="今日计划产量"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="面块输入数量"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="面块包装数量"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="不良产出数量"></BoxTitle></dv-border-box-7></div>
 
-                </div>
-            </dv-border-box-12>
-        </div>
-        <div class="box"><dv-border-box-12>test</dv-border-box-12></div>
-        <div class="box"><dv-border-box-12>asdf</dv-border-box-12></div>
-        <div class="box"><dv-border-box-12>a</dv-border-box-12></div>
+        <!-- Second Line Info -->
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="左机不良产量数量"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="左机包装异常停机次数统计"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="右机包装异常停机次数统计"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="右机不良产量数量"></BoxTitle></dv-border-box-7></div>
 
-        <div class="box"><dv-border-box-6></dv-border-box-6></div>
-        <div class="box"><dv-border-box-6></dv-border-box-6></div>
-        <div class="box"><dv-border-box-6></dv-border-box-6></div>
-        <div class="box"><dv-border-box-6></dv-border-box-6></div>
-
-        <div class="box"><dv-border-box-7></dv-border-box-7></div>
-        <div class="box"><dv-border-box-7></dv-border-box-7></div>
-        <div class="box"><dv-border-box-7></dv-border-box-7></div>
-        <div class="box"><dv-border-box-7></dv-border-box-7></div>
-
+        <!-- Third Line Info -->
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="左机包膜长度不足显示"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="当前产线实时报警提醒"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="其余数据"></BoxTitle></dv-border-box-7></div>
+        <div class="box"><dv-border-box-7><BoxTitle boxTitle="右机包膜长度不足显示"></BoxTitle></dv-border-box-7></div>
     </div>
 </template>
 
 <script>
-export default {
+import BoxTitle from '@/components/BoxTitle.vue'
 
+export default {
+    components:{
+        BoxTitle
+    }
 }
 </script>
 
@@ -39,6 +38,7 @@ export default {
         width: 100%;
         flex-wrap: wrap;
         justify-content: space-evenly;
+        overflow-y: auto;
     }
     @media(max-width: 1080px){
         .Main{
@@ -46,39 +46,46 @@ export default {
         }
     }
     .boxTitle{
-
+        display: flex;
+        justify-content: space-evenly;
+        font-size: medium;
+        letter-spacing: .2rem;
+        color: #01ffff;
     }
     .box{
 		width: 29rem;
 		height: 13rem;
 		/* background-color: lightblue; */
 	}
+    /* 前四个 */
     .box:nth-child(-n+4){
         margin-top: 2%;
     }
+    /* 5-8 */
     .box:nth-child(n+5):nth-child(-n+8){
         height: 20rem;
     }
     .box:nth-child(5){
-        width: 22rem;
+        width: 25rem;
     }
     .box:nth-child(6){
-        width: 36rem;
+        width: 33rem;
     }
     .box:nth-child(7){
-        width: 36rem;
+        width: 33rem;
     }
     .box:nth-child(8){
-        width: 22rem;
+        width: 25rem;
     }
+    /* 9-12 */
     .box:nth-child(n+9):nth-child(-n+12){
         height: 20rem;
         margin-bottom: 5%;
     }
     .box:nth-child(10){
-        width: 45rem;
+        width: 40rem;
     }
     .box:nth-child(11){
-        width: 13rem;   
+        width: 18rem;   
     }
 </style>
